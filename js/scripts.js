ce1ca1cbda5afc762e317ec3e23c146b74bbed3c
Mike Zrimsek <mikezrimsek@gmail.com>
2015-11-02 01:22:48 -0500

    Added fade in to resume sections.  Added some styling to todo list items

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-  $('#resume').show();
+  $('#resume').show(); //resume page shown by default
   $('#todo').hide();
 
+  //nav bar functionality
   $('.nav a').on('click', function() {
     $('.nav').find('.active').removeClass('active');
     var $currentNav = $(this).parent();
@@ -15,4 +16,36 @@ $(document).ready(function() {
       $('#todo').show();
     }
   });
+
+  //initially set all elements to faded out
+  $('#resume .section').each(function() {
+    $(this).css('opacity', '0');
+  });
+
+  //show element if visible on screen
+  fadeIn('#resume .section');
+
+  //fade in elements when they're visible on screen
+  $(window).scroll(function() {
+    fadeIn('#resume .section');
+  });
 });
+
+//return if element is visible on screen
+function isVisible(element) {
+  var objectBottom = $(element).offset().top + $(element).outerHeight()/2;
+  var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+
+  return (objectBottom < windowBottom);
+}
+
+//fade all selected elements in
+function fadeIn(selector) {
+  $(selector).each(function() {
+    if (isVisible($(this)) && $(this).css('opacity') == '0') {
+      $(this).animate({
+        'opacity': '1'
+      }, 500);
+    }
+  });
+}
