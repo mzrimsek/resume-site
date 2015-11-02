ee99e325f03a66f618fb857ce7f40bb1feee5cd3
Mike Zrimsek <mikezrimsek@gmail.com>
2015-11-02 17:09:48 -0500

    Added better documentation to functions

--- js/scripts.js
+++ js/scripts.js
@@ -31,17 +31,24 @@ $(document).ready(function() {
   });
 });
 
-//return if element is visible on screen
+/**
+ * Determine if an element is visible on the screen or not.
+ * @param  {Element}  element The element to check.
+ * @return {Boolean}         True if element is visible.
+ */
 function isVisible(element) {
-  var objectBottom = $(element).offset().top + $(element).outerHeight()/2;
+  var objectBottom = $(element).offset().top + $(element).outerHeight() / 2;
   var windowBottom = $(window).scrollTop() + $(window).innerHeight();
 
   return (objectBottom < windowBottom);
 }
 
-//fade all selected elements in
-function fadeIn(selector) {
-  $(selector).each(function() {
+/**
+ * Fades all not visible elements in when they become visible on the screen.  Elements need to have 'opacity: 0' for any change to be seen.
+ * @param  {String} elements Selector for all elements to be checked to be faded in.
+ */
+function fadeIn(elements) {
+  $(elements).each(function() {
     if (isVisible($(this)) && $(this).css('opacity') == '0') {
       $(this).animate({
         'opacity': '1'
