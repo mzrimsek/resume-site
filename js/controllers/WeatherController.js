2f066d9d1203bd6c3808a45c588376953d9a692c
Mike Zrimsek <mikezrimsek@gmail.com>
2015-11-23 01:07:23 -0500

    Added some comments and updated css with some styles I lost accidentally

--- js/controllers/WeatherController.js
+++ js/controllers/WeatherController.js
@@ -1,6 +1,7 @@
 app.controller('WeatherController', ['$scope', '$http', function($scope, $http) {
   var apiKey = '6fde0a22cc0a46f307fcd213ec2ff92c';
 
+  //variables to hold data returned from API calls
   $scope.currentWeather = {};
   $scope.weatherForecast = {};
   $scope.location = {};
@@ -9,39 +10,39 @@ app.controller('WeatherController', ['$scope', '$http', function($scope, $http) 
     if ($scope.zipcode) {
       $http({
         method: 'GET',
-        url: 'http://api.openweathermap.org/data/2.5/weather?zip=' + $scope.zipcode + ',US&APPID=' + apiKey + '&units=imperial'
-      }).success(function(response) {
-        $scope.currentWeather = response;
-        //console.log($scope.currentWeather);
-
-        $http({
-          method: 'GET',
-          url: 'http://api.openweathermap.org/data/2.5/forecast/daily?id=' + $scope.currentWeather.id + ',US&APPID=' + apiKey + '&cnt=8&units=imperial'
-        }).success(function(response) {
-          var currentDate = new Date();
-          var days = new Array(response.list.length);
-          for (var i = 0; i < days.length; i++) {
-            days[i] = new Day(currentDate.getTime(), response.list[i]);
-            currentDate = new Date(currentDate.setDate(currentDate.getDate() + 1));
-          }
-          $scope.weatherForecast = days;
-          //console.log($scope.weatherForecast);
-        });
-      });
-
-      $http({
-        method: 'GET',
         url: 'http://api.geonames.org/postalCodeSearchJSON?postalcode=' + $scope.zipcode + '&country=US&username=mzrimsek'
       }).success(function(response) {
         $scope.location = response.postalCodes[0];
-        $scope.contentLoaded = true;
         //console.log($scope.location);
+
+        $http({
+          method: 'GET',
+          url: 'http://api.openweathermap.org/data/2.5/weather?zip=' + $scope.zipcode + ',US&APPID=' + apiKey + '&units=imperial'
+        }).success(function(response) {
+          $scope.currentWeather = response;
+          //console.log($scope.currentWeather);
+
+          $http({
+            method: 'GET',
+            url: 'http://api.openweathermap.org/data/2.5/forecast/daily?id=' + $scope.currentWeather.id + ',US&APPID=' + apiKey + '&cnt=8&units=imperial'
+          }).success(function(response) {
+            var currentDate = new Date();
+            var days = new Array(response.list.length);
+            for (var i = 0; i < days.length; i++) {
+              days[i] = new Day(currentDate.getTime(), response.list[i]);
+              currentDate = new Date(currentDate.setDate(currentDate.getDate() + 1));
+            }
+            $scope.weatherForecast = days;
+            //console.log($scope.weatherForecast);
+          });
+        });
+        $scope.contentLoaded = true;
       });
     }
   };
 
-  $scope.getWeatherIcon = function(weatherId){
-    return 'http://openweathermap.org/img/w/'+weatherId+'.png';
+  $scope.getWeatherIcon = function(weatherId) {
+    return 'http://openweathermap.org/img/w/' + weatherId + '.png';
   };
 }]);
 
