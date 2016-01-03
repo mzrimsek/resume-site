c0e299b25ea1abaada7f5518bf03ecf622dad287
Mike Zrimsek <mikezrimsek@gmail.com>
2016-01-03 09:38:26 -0500

    Forgot to add express server file

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var port = 3000;
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.resolve("./dist")));
+
+app.get('/', function(req, res){
+  res.sendFile('index.html');
+});
+
+app.listen(port);
+console.log("Server started on port " + port);
+exports = module.exports = app;
