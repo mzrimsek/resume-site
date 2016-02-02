1576dd60e3f61161016411ef0c3a3d0465c625f3
Mike Zrimsek <mikezrimsek@gmail.com>
2016-02-02 18:14:17 -0500

    Added cors

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var port = 3000;
 
 var app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
