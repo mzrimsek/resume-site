573ca0bd14369185b464b4047bb329d8e61dea2e
Mike Zrimsek <mikezrimsek@gmail.com>
2016-02-05 23:56:27 -0500

    Adding webhook

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.get('/', function(req, res){
   res.sendFile('index.html');
 });
 
+app.post('/deploy/', function(req, res){
+  var spawn = require('child_process').spawn,
+  deploy = spawn('sh', ['./update_server.sh']);
+});
+
 app.listen(port);
 console.log("Server started on port " + port);
 exports = module.exports = app;
